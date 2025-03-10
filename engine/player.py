232e1b659249236bc230e5f85761e3153efbc836
Erbatyr Akyn <erbatyr.akyn.97@gmail.com>
2025-03-11 02:42:18 +0500

    player

--- engine/player.py
+++ engine/player.py
@@ -1,9 +1,8 @@
 from typing import Type, Dict, List
 
-from player_tactical_roles import TacticalRole, Poacher, BoxToBoxMidfielder
-from positions import Position
-from player_attributes import Attribute, Aerial, Crossing, Dribbling, Passing, Shooting, Tackling, Technique, \
-    Aggression, Creativity, Decisions, Leadership, Movement, Positioning, Teamwork, Pace, Stamina, Strength
+from engine.positions import Position
+from player_tactical_roles import TacticalRole, Poacher
+from player_attributes import Attribute
 
 
 class Player:
@@ -33,61 +32,3 @@ class Player:
         attrs = {k.__name__: v.value for k, v in self.attributes.items()}
         return (f"{self.first_name} {self.last_name} ({self.position.value}, Role: {self.tactical_role.name}) - "
                 f"Attributes: {attrs}")
-
-
-player_attributes = {
-    Aerial: Aerial(9),
-    Crossing: Crossing(12),
-    Dribbling: Dribbling(13),
-    Passing: Passing(14),
-    Shooting: Shooting(14),
-    Tackling: Tackling(7),
-    Technique: Technique(18),
-    Aggression: Aggression(11),
-    Creativity: Creativity(14),
-    Decisions: Decisions(14),
-    Leadership: Leadership(8),
-    Movement: Movement(16),
-    Positioning: Positioning(7),
-    Teamwork: Teamwork(15),
-    Pace: Pace(13),
-    Stamina: Stamina(9),
-    Strength: Strength(10)
-}
-player = Player(
-    first_name='Sergei',
-    last_name='Miroshnichenko',
-    attributes=player_attributes,
-    position=Position.CENTRE_FORWARD,
-    tactical_role=Poacher()
-)
-
-available_roles = [Poacher(), BoxToBoxMidfielder()]  # и т.д.
-
-def suggest_best_role(player: Player, roles: List[TacticalRole]) -> TacticalRole:
-    best_score = -1
-    best_role = None
-
-    for role in roles:
-        if player.position not in role.suitable_positions:
-            continue  # Пропускаем, если роль не подходит по позиции
-
-        total = 0
-        count = 0
-        for attr_type in role.key_attributes:
-            total += player.get_attribute(attr_type)
-            count += 1
-
-        if count == 0:
-            continue
-
-        score = total / count
-
-        if score > best_score:
-            best_score = score
-            best_role = role
-
-    return best_role
-
-suggested_role = suggest_best_role(player, available_roles)
-print(f"Лучшая роль для {player.full_name}: {suggested_role.name}")

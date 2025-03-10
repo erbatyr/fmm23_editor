75e4635098439af828c44ed85043be251c5067fa
Erbatyr Akyn <erbatyr.akyn.97@gmail.com>
2025-03-10 06:42:30 +0500

    tactical_roles: poacher was added and some code cleaning

--- engine/player_tactical_roles.py
+++ engine/player_tactical_roles.py
@@ -1,29 +1,31 @@
-from abc import abstractmethod
-from dataclasses import dataclass
 from typing import List, Type
 
-from fm_mine.engine.player import Player
-from fm_mine.engine.player_attributes import Attribute
+from player_attributes import Attribute
 
 
-@dataclass
 class TacticalRole:
     name: str
     suitable_positions: List[str]
-    key_attributes: List[Type[Attribute]]
+    key_attributes: List[Type['Attribute']]
 
-    def modify_attributes(self, player: Player):
+    def modify_attributes(self, player: 'Player'):
         """Изменяет характеристики игрока в зависимости от роли"""
         pass
 
 
+class Poacher(TacticalRole):
+    def modify_attributes(self, player: 'Player'):
+        # player.modify_attribute(Passing, 7)
+        # player.modify_attribute(Creativity, 5)
+        # player.modify_attribute(Pace, -2)
+        pass
 
 class BoxToBoxMidfielder(TacticalRole):
-    def modify_attributes(self, player: Player):
+    def modify_attributes(self, player: 'Player'):
         pass
 
 class DeepLyingPlaymaker(TacticalRole):
-    def modify_attributes(self, player: Player):
+    def modify_attributes(self, player: 'Player'):
         # player.modify_attribute(Passing, 7)
         # player.modify_attribute(Creativity, 5)
         # player.modify_attribute(Pace, -2)

ece22d3a06d28755d3552cfe821839908ca29fa1
Erbatyr Akyn <erbatyr.akyn.97@gmail.com>
2025-03-10 06:40:53 +0500

    positions: str and group was added

--- engine/positions.py
+++ engine/positions.py
@@ -18,3 +18,19 @@ class Position(Enum):
     LEFT_INSIDE_FORWARD = "LIF"
     RIGHT_INSIDE_FORWARD = "RIF"
     CENTRE_FORWARD = "CF"
+
+    def __str__(self):
+        return self.value
+
+
+    def group(self):
+        if self in {Position.GOALKEEPER}:
+            return "Goalkeeper"
+        elif self in {Position.RIGHT_BACK, Position.LEFT_BACK, Position.CENTER_BACK, Position.RIGHT_WING_BACK,
+                      Position.LEFT_WING_BACK}:
+            return "Defender"
+        elif self in {Position.DEFENSIVE_MIDFIELDER, Position.CENTRAL_MIDFIELDER, Position.RIGHT_MIDFIELDER,
+                      Position.LEFT_MIDFIELDER, Position.ATTACKING_MIDFIELDER}:
+            return "Midfielder"
+        else:
+            return "Forward"
